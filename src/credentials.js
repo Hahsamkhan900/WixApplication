02f9eb9f0361aafdf7dbb7b99a00523ffdbcbd1b
Hahsamkhan900 <87896527+Hahsamkhan900@users.noreply.github.com>
2022-02-12 00:43:49 +0500

    Add files via upload

---
+++ src/credentials.js
@@ -0,0 +1,4 @@
+// TODO: Update APP_SECRET with your application secret key (can be found in Wix Developers under Workspace/OAuth)
+const APP_SECRET = '256cf4cd-96b6-418e-9ad9-2ecbfa5a38af';
+
+exports.APP_SECRET = APP_SECRET;
